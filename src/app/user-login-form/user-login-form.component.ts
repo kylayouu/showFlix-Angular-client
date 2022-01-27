6a0e1d125a8e87c526ea8f8296ae40e6d566314e
Kyla Quilos <quilos.kyla@gmail.com>
2022-01-26 17:28:13 -0800

    implemented router

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -9,6 +9,8 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -21,7 +23,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -38,6 +41,7 @@ export class UserLoginFormComponent implements OnInit {
     this.snackBar.open('User login successful', 'OK', {
         duration: 2000
     });
+    this.router.navigate(['movies']);
     }, (result) => {
       console.log(result);
       this.snackBar.open('User login successful', 'OK', {
